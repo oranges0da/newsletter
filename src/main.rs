1f06e90468a4cf28e5d65c7d0b41e6acf001781f
Alex Ionitel <oranggsoda@gmail.com>
2023-05-09 17:52:39 -0400

    hello_world example working with TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod server;
+
+use server::run;
+use std::net::TcpListener;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
---
+++ src/server.rs
@@ -0,0 +1,15 @@
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
+
+// main entry point for main to start instance of server
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/hello_world", web::get().to(hello_world)))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
+
+async fn hello_world() -> HttpResponse {
+    HttpResponse::Ok().body("Hello There!")
+}
