fc6d625117211945cfd1c38b3b60bd9b929afd9b
Alex Ionitel <oranggsoda@gmail.com>
2023-05-11 01:20:48 -0400

    script to create postgres db on docker working

--- src/server.rs
+++ src/server.rs
@@ -3,13 +3,13 @@ use std::net::TcpListener;
 
 // main entry point for main to start instance of server
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(hello_world)))
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(listener)?
         .run();
 
     Ok(server)
 }
 
-async fn hello_world() -> HttpResponse {
-    HttpResponse::Ok().body("Hello There!")
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().body("Health check working!")
 }
