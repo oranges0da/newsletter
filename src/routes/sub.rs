18376d037559b4f933f44c70f7770c3266d5a9ee
Alex Ionitel <oranggsoda@gmail.com>
2023-05-23 22:47:05 -0400

    removed unnecessary test

--- src/routes/sub.rs
+++ src/routes/sub.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
+use sqlx::PgConnection;
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -8,6 +9,6 @@ pub struct FormData {
 }
 
 // if form data valid return 200 OK
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(_form: web::Form<FormData>, _conn: web::Data<PgConnection>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
--- tests/postgres.rs
+++ tests/postgres.rs
@@ -1,17 +1,7 @@
 use newsletter::config;
 use sqlx::{Connection, PgConnection};
 
-// test to make sure we can connect to postgres
-#[tokio::test]
-async fn connect_to_postgres() {
-    let config = config::get_config().expect("Failed to read config in conn_postgres");
-    let conn_string = config.db_settings.get_connection_string();
-
-    PgConnection::connect(&conn_string)
-        .await
-        .expect("Failed to connect to postgres.");
-}
-
+// make sure we can connect and query to postgres
 #[tokio::test]
 async fn query_postgres() {
     let config = config::get_config().expect("Failed to read config in conn_postgres");
